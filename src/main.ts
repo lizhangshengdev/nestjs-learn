b00e74fb7caeeabe7871c86385e74010aeaac7aa
lzs2018 <lzs2609534520@gmail.com>
2024-07-15 21:29:09 +0800

    [新增]：swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder,SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,17 @@ async function bootstrap() {
       transform:true
     }
   ))
+
+  const config = new DocumentBuilder()
+  .setTitle('Api文档')
+  .setDescription('创作者: Mikey')
+  .addServer('localhost:3000')
+  .setVersion('1.0').build();
+
+  const document = SwaggerModule.createDocument(app,config);
+
+  SwaggerModule.setup('api',app,document)
+
   await app.listen(3000);
 }
 bootstrap();
